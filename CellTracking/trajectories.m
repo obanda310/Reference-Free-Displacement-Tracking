8578473cb0c3ef88ef8c35b740cd0e0e6cd649e8
Janty <jshoga@udel.edu>
2016-04-11 14:59:20 -0400

    Gets rid of unnecessary code

--- CellTracking/trajectories.m
+++ CellTracking/trajectories.m
@@ -82,7 +82,7 @@ end
 %3.) Associating objects to rows by identifying nearest right neighbor 
 %--------------------------------------------------------------------------
 objAll = [x_0(:,1),resY-y_0(:,1)];
-rows = zeros(2,1);
+rows = [];
 row = 1;
 col = 1;
 % distances = cell(numObj,3);
@@ -91,36 +91,30 @@ for i = 1:numTraj
     if book1(11,1,i) ~= 1
         i=i+1;
     else    
-    thisObj = [x_0(i,1),resY-y_0(i,1)];
-    objCount = (1:numTraj)';
-    distances = sqrt(sum(bsxfun(@minus,objAll,thisObj).^2,2));
-    objData = [objCount,distances];
-    sortDist = sortrows(objData,2);
-    neighbors(1,1:4) = sortDist(2:5,1);
-    nDistance = sortDist(2:5,2);
-    for j = 1:4
-        n(j,1:2) = objAll(neighbors(1,j),:);
-        n(j,3) = neighbors(j);
-    end
-    a = bsxfun(@minus,n(:,1:2),thisObj);
-    b = mean([min(abs(a)),max(abs(a))]);
-    goodNeighbors = n(abs(a(:,2)) < b & a(:,1) > 0,:);
-    if size(goodNeighbors,1) > 1
-        sortN = sortrows(goodNeighbors,1);
-        rightNeighbor = sortN(1,:);
-    else
-        rightNeighbor = goodNeighbors;
-    end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    if isempty (rows)                       %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        rows(row,col) = i;                  %%%%% This never happens %%%%%%
-        book1(10,:,i)= rightNeighbor(3);    %%%%%% This never happens %%%%%
-        book1(9,:,i) = row;                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    else
+        thisObj = [x_0(i,1),resY-y_0(i,1)];
+        objCount = (1:numTraj)';
+        distances = sqrt(sum(bsxfun(@minus,objAll,thisObj).^2,2));
+        objData = [objCount,distances];
+        sortDist = sortrows(objData,2);
+        neighbors(1,1:4) = sortDist(2:5,1);
+        nDistance = sortDist(2:5,2);
+        for j = 1:4
+            n(j,1:2) = objAll(neighbors(1,j),:);
+            n(j,3) = neighbors(j);
+        end
+        a = bsxfun(@minus,n(:,1:2),thisObj);
+        b = mean([min(abs(a)),max(abs(a))]);
+        goodNeighbors = n(abs(a(:,2)) < b & a(:,1) > 0,:);
+        if size(goodNeighbors,1) > 1
+            sortN = sortrows(goodNeighbors,1);
+            rightNeighbor = sortN(1,:);
+        else
+            rightNeighbor = goodNeighbors;
+        end
         if isempty(rightNeighbor)   %if a right neighbor doesn't exist for the current trajectory (i.e. if it is on the edge of the frame)
-            col = col + 1;          %adjust the current column to the next slot(to the right), and
+                                    %adjust the current column to the next slot(to the right), and
             rows(row,col) = i;      %place the current trajectory in that slot
+            col = col + 1;
             book1(9,:,i) = row;
             row = row + 1;          %then start a new row for the next trajectory in the list (because we have reached an edge)
             col = 1;                %start at the beginning of the new row for the next trajectory
@@ -132,21 +126,19 @@ for i = 1:numTraj
                 [trow,tcol] = find(rows == rightNeighbor(3));    %what row does the right neighbor belong to? Store as 'trow'
                 emptyColumn = size(rows,2)+1;                    %Add one to the width of the 'rows' array to have a slot which will necessarily be empty, stored as 'emptyColumn'
                 if rows(trow,emptyColumn-1) == 0                 %If there is an empty value to the left of 'emptyColumn' in 'trow' then,
-                rows(trow,emptyColumn-1) = i;                    %store trajectory number in that empty slot
+                    rows(trow,emptyColumn-1) = i;                %store trajectory number in that empty slot
                 else                                             %otherwise,
-                rows(trow,emptyColumn) = i;                      %place the trajectory number into a new empty column
+                    rows(trow,emptyColumn) = i;                  %place the trajectory number into a new empty column
                 end
                 book1(10,:,i)= rightNeighbor(3);
                 book1(9,:,i) = trow;
-            else
-                col = col + 1;                                   %if the right neighbor is not on the list of rows yet, then move one column right and
-                rows(row,col) = i;                               %add the current trajectory to the empty slot
-                book1(10,:,i)= rightNeighbor(3);                 %add rightNeighbor to index book1 (index#10)
+            else                                                %if the right neighbor is not on the list of rows yet, then move one column right and
+                rows(row,col) = i;                              %add the current trajectory to the empty slot
+                col = col + 1;
+                book1(10,:,i)= rightNeighbor(3);                %add rightNeighbor to index book1 (index#10)
                 book1(9,:,i) = row;
             end
-        end
-    end
-    
+        end    
     end
 end
 
