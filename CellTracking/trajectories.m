5bf01ca20c98580324f1df9152016f3616512e83
Janty <jshoga@udel.edu>
2016-04-22 13:39:14 -0400

    Adds leftNeighbor to improve row designation of trajectories

--- CellTracking/trajectories.m
+++ CellTracking/trajectories.m
@@ -26,25 +26,34 @@ for i = 1:numTraj
     %object/trajectory per page. Most of the code is to ensure that each
     %page is the same size matrix as the next.
     
-    tempObj = num(traj==i,:); %stores data to 'tempObj' pertaining to all frames of one object/trajectory
-    numFrames = size(tempObj,1); % number of frames that the current object appears in.
-    startFrame = min(tempObj(:,3)); % the first frame that an object appears in (tracking software starts at 0)
-    endFrame = (startFrame+numFrames); % the last frame that an object appears in
+    % stores data to 'tempObj' pertaining to all frames of one
+    % object/trajectory
+    tempObj = num(traj==i,:);
+    % number of frames that the current object appears in.
+    numFrames = size(tempObj,1);
+    % the first frame that an object appears in (tracking software starts 
+    % at 0
+    startFrame = min(tempObj(:,3));
+    % the last frame that an object appears in
+    endFrame = (startFrame+numFrames);
     book1(11,:,i) = startFrame+1;
     book1(12,:,i) = endFrame;
-    if startFrame > 0  % this statement fills in the upper portion of obj matrix with zeros if the first frame is not 0  
+    % this fills in the upper portion of obj matrix with zeros if the first
+    % frame is not 0
+    if startFrame > 0  
         obj = [zeros(startFrame,12);tempObj];
     else
         if numFrames < totalNumFrames
         obj = [tempObj;zeros(totalNumFrames-numFrames,12)];
         else
         if startFrame == 0
-        obj = tempObj; %this statement applys to objects that start in frame 0
+        obj = tempObj; %this applies to objects that start in frame 0
         end
         end
     end
     if i == 1
-        book2 = zeros(totalNumFrames, 12); %'book1' is our book (3D array) of object data
+        %'book1' is our book (3D array) of object data
+        book2 = zeros(totalNumFrames, 12);
         book2(:,:) = obj;
     else
         book2 = cat(3,book2,obj); %adding pages of 'obj' data to the book.
@@ -82,7 +91,7 @@ end
 %3.) Associating objects to rows by identifying nearest right neighbor 
 %--------------------------------------------------------------------------
 objAll = [x_0(:,1),resY-y_0(:,1)];
-rows = [];
+rows = zeros(2,1);
 row = 1;
 col = 1;
 % distances = cell(numObj,3);
@@ -91,80 +100,102 @@ for i = 1:numTraj
     if book1(11,1,i) ~= 1
         i=i+1;
     else    
-        thisObj = [x_0(i,1),resY-y_0(i,1)];
-        objCount = (1:numTraj)';
-        distances = sqrt(sum(bsxfun(@minus,objAll,thisObj).^2,2));
-        objData = [objCount,distances];
-        sortDist = sortrows(objData,2);
-        neighbors(1,1:4) = sortDist(2:5,1);
-        nDistance = sortDist(2:5,2);
-        for j = 1:4
-            n(j,1:2) = objAll(neighbors(1,j),:);
-            n(j,3) = neighbors(j);
-        end
-        a = bsxfun(@minus,n(:,1:2),thisObj);
-        b = mean([min(abs(a)),max(abs(a))]);
-        goodNeighbors = n(abs(a(:,2)) < b & a(:,1) > 0,:);
-        if size(goodNeighbors,1) > 1
-            sortN = sortrows(goodNeighbors,1);
-            rightNeighbor = sortN(1,:);
-        else
-            rightNeighbor = goodNeighbors;
-        end
+    thisObj = [x_0(i,1),resY-y_0(i,1)];
+    objCount = (1:numTraj)';
+    distances = sqrt(sum(bsxfun(@minus,objAll,thisObj).^2,2));
+    objData = [objCount,distances];
+    sortDist = sortrows(objData,2);
+    neighbors(1,1:4) = sortDist(2:5,1);
+    nDistance = sortDist(2:5,2);
+    for j = 1:4
+        n(j,1:2) = objAll(neighbors(1,j),:);
+        n(j,3) = neighbors(j);
+    end
+    a = bsxfun(@minus,n(:,1:2),thisObj);
+    b = mean([min(abs(a)),max(abs(a))]);
+    goodNeighbors = n(abs(a(:,2)) < b & a(:,1) > 0,:);
+    if size(goodNeighbors,1) > 1
+        sortN = sortrows(goodNeighbors,1);
+        rightNeighbor = sortN(1,:);
+    else
+        rightNeighbor = goodNeighbors;
+    end
+    goodNeighborsForLeft = n(abs(a(:,2)) < b & a(:,1) < 0,:);
+    if size(goodNeighborsForLeft,1) > 1
+        sortN = sortrows(goodNeighborsForLeft,1);
+        leftNeighbor = sortN(1,:);
+    else
+        leftNeighbor = goodNeighborsForLeft;
+    end
+    
         if isempty(rightNeighbor)   %if a right neighbor doesn't exist for the current trajectory (i.e. if it is on the edge of the frame)
-                                    %adjust the current column to the next slot(to the right), and
+            col = col + 1;          %adjust the current column to the next slot(to the right), and
             rows(row,col) = i;      %place the current trajectory in that slot
-            col = col + 1;
             book1(9,:,i) = row;
             row = row + 1;          %then start a new row for the next trajectory in the list (because we have reached an edge)
             col = 1;                %start at the beginning of the new row for the next trajectory
             book1(10,:,i)= 0;
-        else                                                     %if we are NOT at the end of the row, we assume first that we MAY NOT be in the correct row, 
-                                                                 %we check the current trajectory's right neighbor's row to see if it exists 
-            rowCheck = ismember(rightNeighbor(3),rows);          %is the right neighbor to current object already assigned a row? store as 'rowCheck'
-            if rowCheck == 1                                     %if yes then
+        elseif isempty(leftNeighbor)   %if a left neighbor doesn't exist for the current trajectory (i.e. the first member of a row)
+            col = 1;          %adjust the current column to the first column
+            rows(row,col) = i;      %place the current trajectory in that slot
+            book1(9,:,i) = row;
+            col = 1;                %start at the beginning of the new row for the next trajectory
+            book1(10,:,i)= 0;
+        else                                                     %if we are NOT at the beginning or end of the row, we assume first that we MAY NOT be in the correct row, 
+                                                                 %we check the current trajectory's right/left neighbor's row to see if they exist 
+            rowCheckRight = ismember(rightNeighbor(3),rows);
+            rowCheckLeft = ismember(leftNeighbor(3),rows);       %is the right neighbor to current object already assigned a row? store as 'rowCheck'
+            if rowCheckRight == 1                                %if right exists then
                 [trow,tcol] = find(rows == rightNeighbor(3));    %what row does the right neighbor belong to? Store as 'trow'
-                emptyColumn = size(rows,2)+1;                    %Add one to the width of the 'rows' array to have a slot which will necessarily be empty, stored as 'emptyColumn'
-                if rows(trow,emptyColumn-1) == 0                 %If there is an empty value to the left of 'emptyColumn' in 'trow' then,
-                    rows(trow,emptyColumn-1) = i;                %store trajectory number in that empty slot
-                else                                             %otherwise,
-                    rows(trow,emptyColumn) = i;                  %place the trajectory number into a new empty column
-                end
+                emptyColumn = size(rows,2)+1;                    %Add one to the width of the 'rows' array to have a slot which will necessarily be empty, stored as 'emptyColumn'                            
+                    while rows(trow,emptyColumn-1) == 0          %If there is an empty value to the left of 'emptyColumn' in 'trow' then,
+                        emptyColumn = emptyColumn-1;
+                    end
+                rows(trow,emptyColumn) = i;                      %place the trajectory number into a new empty column           
                 book1(10,:,i)= rightNeighbor(3);
                 book1(9,:,i) = trow;
-            else                                                %if the right neighbor is not on the list of rows yet, then move one column right and
-                rows(row,col) = i;                              %add the current trajectory to the empty slot
                 col = col + 1;
-                book1(10,:,i)= rightNeighbor(3);                %add rightNeighbor to index book1 (index#10)
+            elseif rowCheckLeft == 1                             %if left exists then                 
+                [trow,tcol] = find(rows == leftNeighbor(3));     %what row does the right neighbor belong to? Store as 'trow'
+                emptyColumn = size(rows,2)+1;                    %Add one to the width of the 'rows' array to have a slot which will necessarily be empty, stored as 'emptyColumn'                              
+                    while rows(trow,emptyColumn-1) == 0          %If there is an empty value to the left of 'emptyColumn' in 'trow' then,
+                        emptyColumn = emptyColumn-1;
+                    end             
+                rows(trow,emptyColumn) = i;                      %place the trajectory number into a new empty column
+                book1(9,:,i) = trow;
+                col = col + 1;             
+            else
+                col = col + 1;                                   %if the right neighbor is not on the list of rows yet, then move one column right and
+                rows(row,col) = i;                               %add the current trajectory to the empty slot
+                book1(10,:,i)= rightNeighbor(3);                 %add rightNeighbor to index book1 (index#10)
                 book1(9,:,i) = row;
             end
-        end    
+        end
     end
 end
-
 %%
 %--------------------------------------------------------------------------
 %4.)Identifying deviations from zero state in later frames
 %--------------------------------------------------------------------------
-%Now that we have a list of rows of trajectories we need to find the
-%error displacement for members of a particular row caused by irregularities in the patterning process. 
+% Now that we have a list of rows of trajectories we need to find the
+% error displacement for members of a particular row caused by 
+% irregularities in the patterning process. 
 %
-%We are measuring from 
-%the zero frame to
-%the current frame. 
+% We are measuring from the zero frame to the current frame. 
 %
-%We can do this by rounding the x and y displacement of each value to the nearest
-%tenth and determining the mode of each row in a frame. These modes can be
-%used to identify the displacements that occur the most which which we will assume
-%to be the case for displacement caused by patterning error, and not by a
-%cell seeded on top of the gel.
+% We can do this by rounding the x and y displacement of each value to the 
+% nearest tenth and determining the mode of each row in a frame. These 
+% modes can be used to identify the displacements that occur the most which
+% which we will assume to be the case for displacement caused by patterning
+% error, and not by a cell seeded on top of the gel.
 %
-%We then take the average value of x and y displacement of every row member
-%yielding the x AND y mode values and substract those values from the starting x and
-%y positions of the trajectories in the current test frame.
+% We then take the average value of x and y displacement of every row 
+% member yielding the x AND y mode values and substract those values from 
+% the starting x and y positions of the trajectories in the current test 
+% frame.
 %
-%By shifting the start position of the vectors, we can remove the error in
-%displacement caused by patterning irregularities
+% By shifting the start position of the vectors, we can remove the error in
+% displacement caused by patterning irregularities
 
 numTrajInRows = max(rows(:));
 rowElements = find(book1(9,1,:)==1);
@@ -174,14 +205,14 @@ bookY = zeros(numRowElements+1,max(book1(9,1,:)),totalNumFrames);
 for f=1:totalNumFrames
     m=2;
     for i=1:numTrajInRows
-    j=book1(9,f,i);
-    k=book1(7,f,i);
-    l=book1(8,f,i);
-    rowElements = find(book1(9,1,:)==j);
-    numRowElements = numel(rowElements);
-    bookX(m-((j-1)*numRowElements),j,f)= k;
-    bookY(m-((j-1)*numRowElements),j,f)= l;
-    m=m+1;
+        j=book1(9,f,i);
+        k=book1(7,f,i);
+        l=book1(8,f,i);
+        rowElements = find(book1(9,1,:)==j);
+        numRowElements = numel(rowElements);
+        bookX(m-((j-1)*numRowElements),j,f)= k;
+        bookY(m-((j-1)*numRowElements),j,f)= l;
+        m=m+1;
     end
 end
 
