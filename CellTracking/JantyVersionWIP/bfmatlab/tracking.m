dbf235c42b71b7d56ce09234a366a25a170cde6c
obanda310 <obanda310@gmail.com>
2016-10-14 16:55:49 -0400

    Divides maxima into planes

--- CellTracking/JantyVersionWIP/bfmatlab/tracking.m
+++ CellTracking/JantyVersionWIP/bfmatlab/tracking.m
@@ -17,22 +17,22 @@ noImgs = size(images,3);
 ppImages5 = bwareaopen(ppImages5,20);
 ShowStack(ppImages5,centroids2)
 %% Final Pre-Processing Before Finding Local Maxima
-% Create ppImages 9 to fill holes in z-dimension
-[ppImages6,rect2] = EditStack(ppImages5,centroids2);
+% Create ppImages 9 to fill holes in z-dimension. Double click after making
+% selection to crop or an error will occur.
+[ppImages6,cropImages2, rect2] = EditStack(ppImages5,images,centroids2);
 
 %% Finding 3D Local Maxima
 % Create a gaussian filtered version of original to decrease false local
 % maxima
 d = 3;
 sig1 = 1/(1+sqrt(2))*d;
-sig2 = sqrt(2) * sig1;
-parfor i = 1:noImgs
-    ppImages7(:,:,i) = imgaussfilt(images(rect2(1,2):rect2(1,2)+rect2(1,4),rect2(1,1):rect2(1,1)+rect2(1,3),i),sig2);
-end
+sig2 = sqrt(2) * sig1; 
+
 % Multiply the gaussian image by the mask image to isolate regions of
 % interest
-ppImages8 = ppImages6.*ppImages7;
+ppImages8 = ppImages6.*imgaussfilt(cropImages2,sig2);
 ShowStack(ppImages8,centroids2)
+
 % Find local maxima in 3D (pixel resolution)
 ppImages9 = imregionalmax(ppImages8);
 
@@ -43,7 +43,7 @@ ppImages9 = imregionalmax(ppImages8);
 
 % Find subpixel maxima based on initial guesses from imregionalmax on the
 % original images
-[rsM,csM,ssM] = subpix3d(rM,cM,sM,images);
+[rsM,csM,ssM] = subpix3d(rM,cM,sM,cropImages2);
 
 % 3D plot subpixel local maxima
 figure
@@ -52,10 +52,45 @@ scatter3(rsM,csM,ssM,'.')
 %%%%%%%%%%%%%%%%%%%%%% END OMAR'S ADDITIONS 10/12/16 %%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Dividing maxima by plane
+%Find the center of each z plane of dots based on histogram of local 3D
+%maxima
 [~,zCenters] = find(imregionalmax(imgaussfilt(histcounts(sM,noImgs),3)));
-for i = 1:size(zCenters,2)
-    
+
+%Find the spacing (# of frames) between each plane 
+zSpacing = zeros(1,size(zCenters,2)-1);
+for i = 1:(size(zCenters,2)-1)
+    zSpacing(1,i) = zCenters(1,i+1)-zCenters(1,i);
 end
+
+%Choose a 'tail' size around plane centers for associating local maxima
+%above and below the plane of interest. 
+zTails = round((mean(zSpacing(1,:)))/4);
+
+%Assign local maxima to planes
+zPlaneIndices = zeros(size(zCenters,2),2);
+for i = 1:size(zCenters,2)
+    zPlaneIndices(i,1) = min(find(sM>(zCenters(1,i)-zTails)));
+    zPlaneIndices(i,2) = max(find(sM<(zCenters(1,i)+zTails)));
+    zPlaneIndices(i,3) = zPlaneIndices(i,2)-zPlaneIndices(i,1);
+end
+
+zSortedMaxima = zeros(max(zPlaneIndices(:,3)),3,size(zCenters,2));
+for i = 1:size(zCenters,2)
+    zSortedMaxima(1:zPlaneIndices(i,3)+1,1,i) = rM(zPlaneIndices(i,1):zPlaneIndices(i,2),1);
+    zSortedMaxima(1:zPlaneIndices(i,3)+1,2,i) = cM(zPlaneIndices(i,1):zPlaneIndices(i,2),1);
+    zSortedMaxima(1:zPlaneIndices(i,3)+1,3,i) = sM(zPlaneIndices(i,1):zPlaneIndices(i,2),1);
+end
+
+for i = 1:size(zCenters,2)
+    temp = zeros(zPlaneIndices(i,3),3);
+    temp(1:zPlaneIndices(i,3),:) = zSortedMaxima(1:zPlaneIndices(i,3),:,i);
+    fitobject{i} = fit([temp(:,1),temp(:,2)],temp(:,3),'poly11');
+    plot(fitobject{i},[temp(:,1),temp(:,2)],temp(:,3))
+    hold on
+end
+
+%% Predict New Local 3D maxima by Plane
+
 
 %% image pre-processing
 % something in here is making the features larger and blur into each other
