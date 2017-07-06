e1a050b84d06c2ae352341e92b7627d28b8dfe9d
obanda310 <obanda310@gmail.com>
2017-07-06 17:16:40 -0400

    Updates Manual Corrections to a working Prototype

--- CellTracking/trackingOutSourced.m
+++ CellTracking/trackingOutSourced.m
@@ -53,7 +53,7 @@ for i = 1:size(roiMasks,3)
     temp(temp>0) = high*(1);
     temp = (temp/high)*(i^4/size(roiMasks,3)^4);
     
-roiMasks2(:,:,i) = temp;
+    roiMasks2(:,:,i) = temp;
 end
 maxMasks = permute(max(permute(roiMasks2,[3 1 2])),[2 3 1]);
 pillarView = figure;
@@ -74,7 +74,7 @@ for i = 1:size(roiImgs,3)
     else
         temp(:,6) = i;
         subpixMaxima3 = cat(1,subpixMaxima3,temp);
-    end    
+    end
 end
 
 %% Object Detection Plot
@@ -115,198 +115,236 @@ disp('Linking Pillars')
 
 
 %%
-% 
+%
 % % Cleaning tracks and fixing incorrect matches
 maxAi = 45;
 maxAc = 90;
 lub(:,8) = 1:1:size(lub,1);
 
 maxD = 5/pixelSize;
+% %
+% for k = 1:4
+% numPillars = max(lub(:,7));
+% [lub] = refreshData(lub,maxAi,maxAc,numPillars,maxMasks);
+% problems = find(lub(:,20)>0);
+% %
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-for k = 1:4
+% % Fixing Problem Pillars
+% sLocs = unique(lub(:,[10 11 7]),'rows','stable');
+% exempt = unique(lub((lub(:,21)<(0.8*size(roiImgs,3))),7)); % exempt from being matched to because the pillar is too short
+% for i = 1:size(exempt,1)
+% sLocs(sLocs(:,3)==exempt(i,1),:) = 0;
+% end
+% sLocs = unique(sLocs,'rows','stable');
+% sLocs(sLocs(:,3)==0,:) = [];
+% lub(:,23) = lub(:,7);
+%
+% [lub] = fixProblemPillars(lub,maxAi,problems,sLocs,maxD);
+%
+%
+% end
+lub = sortrows(lub,[7 6]);
 numPillars = max(lub(:,7));
 [lub] = refreshData(lub,maxAi,maxAc,numPillars,maxMasks);
 problems = find(lub(:,20)>0);
 
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Fixing Problem Pillars
-sLocs = unique(lub(:,[10 11 7]),'rows','stable');
-exempt = unique(lub((lub(:,21)<(0.8*size(roiImgs,3))),7)); % exempt from being matched to because the pillar is too short
-for i = 1:size(exempt,1)
-sLocs(sLocs(:,3)==exempt(i,1),:) = 0;
+%% Visualizing Problem Pillars
+% figure
+% imshow(maxMasks)
+% hold on
+% scatter3(lub(problems,1),lub(problems,2),lub(problems,6))
+% clear tempInd1 tempInd2
+% for j = 1:max(lub(:,7))
+%     clear tempPillar
+%     tempInd1 = find(lub(:,7)==j,1,'first');
+%     tempInd2 = find(lub(:,7)==j,1,'last');
+%     if((tempInd2-tempInd1)>0)
+%         tempPillar = lub(tempInd1:tempInd2,:);
+%         plot3(tempPillar(:,1),tempPillar(:,2),tempPillar(:,6))
+%         %plot3(tempPillar(:,24),tempPillar(:,25),tempPillar(:,6))
+%     end
+% end
+% hold off
+%% Automated Problem Pillar Selection with Manual Track Adjustment
+problems(:,3) = 0;
+for j = 1:2
+for l = 1:size(problems,1)
+    if problems(l,3) ~= 1
+        clear cntrPt pPilIdx
+        pPilIdx = problems(l,1);
+        pPilIdcs = find(lub(problems(:,1),7)==lub(problems(l,1),7))
+        cntrPt = round(lub(pPilIdx,1:2));
+        fSize = 50; % Distance from center point to make frame
+        nSize = 20; % max neighbor distance
+        
+        %determine if object is near the edge of the main frame
+        if size(maxMasks,2)-cntrPt(1,1) < fSize
+            fSizeXmin = fSize;fSizeXmax = (size(maxMasks,2)-cntrPt(1,1))-1;
+        elseif cntrPt(1,1) <= fSize
+            fSizeXmin = cntrPt(1,1)-1;fSizeXmax = fSize;
+        else
+            fSizeXmin = fSize;fSizeXmax = fSize;
+        end
+        
+        if size(maxMasks,1)-cntrPt(1,2) < fSize
+            fSizeYmin = fSize;fSizeYmax = (size(maxMasks,1)-cntrPt(1,2))-1;
+        elseif cntrPt(1,2) <= fSize
+            fSizeYmin = cntrPt(1,2)-1;fSizeYmax = fSize;
+        else
+            fSizeYmin = fSize;fSizeYmax = fSize;
+        end
+        
+        pPF = figure('Position',[100 100 800 800]);
+        handles.axes1 = axes( ...
+            'Units','normalized', ...
+            'Position',[0 0 1 1]);
+        imshow(maxMasks(cntrPt(1,2)-fSizeYmin:cntrPt(1,2)+fSizeYmax,cntrPt(1,1)-fSizeXmin:cntrPt(1,1)+fSizeXmax))
+        hold on
+        pbd = uicontrol('style','pushbutton','units','pixels','position',[120,5,70,20],'string','delete','Callback',@pbDelete);
+        pbs = uicontrol('style','pushbutton','units','pixels','position',[120,25,70,20],'string','skip','Callback',@pbSkip);
+        
+       
+        %Plot Problem
+        scatter3(fSizeXmin,fSizeYmin,lub(pPilIdx,6),'g')
+        
+        %Plot neighbors and neighbor pillars
+        nghbrs = unique(lub((lub(:,1)>(cntrPt(1,1)-nSize)&lub(:,1)<(cntrPt(1,1)+nSize)&lub(:,2)>(cntrPt(1,2)-nSize)&lub(:,2)<(cntrPt(1,2)+nSize)),7));
+        for i = 1:size(nghbrs,1)
+            if size(find(lub(:,7)==nghbrs(i,1) & lub(:,6)<lub(pPilIdx,6),1,'last'),1)>0
+                nghbrs(i,2) = find(lub(:,7)==nghbrs(i,1) & lub(:,6)<lub(pPilIdx,6),1,'last');
+                scatter3(lub(nghbrs(i,2),1)-(cntrPt(1,1)-fSizeXmin),lub(nghbrs(i,2),2)-(cntrPt(1,2)-fSizeYmin),lub(nghbrs(i,2),6),'r')
+                trckNum = num2str(nghbrs(i,1));
+                tempInd1 = find(lub(:,7)==nghbrs(i,1));
+                if size(tempInd1,1)>0
+                    tempPillar = lub(tempInd1,:);
+                    tempPillar = sortrows(tempPillar,6);
+                    plot3(tempPillar(:,1)-(cntrPt(1,1)-fSizeXmin),tempPillar(:,2)-(cntrPt(1,2)-fSizeYmin),tempPillar(:,6))
+                    %plot3(tempPillar(:,24),tempPillar(:,25),tempPillar(:,6))
+                end
+                pb(i) = uicontrol('style','pushbutton','units','pixels','position',[40,5+(20*i),70,20],'string',num2str(trckNum),'Callback',@pbPillarChoice);
+                
+                trckText = strcat('\leftarrow ',trckNum);
+                text(lub(nghbrs(i,2),1)-(cntrPt(1,1)-fSizeXmin),lub(nghbrs(i,2),2)-(cntrPt(1,2)-fSizeYmin),lub(nghbrs(i,2),6),trckText,'Color','red')
+            else
+                nghbrs(i,2) = 0;
+            end
+        end
+         %Plot Problem's Current Pillar
+        tempInd1 = find(lub(:,7)==lub(pPilIdx,7));
+        if size(tempInd1,1)>0
+            tempPillar = lub(tempInd1,:);
+            tempPillar = sortrows(tempPillar,6);
+            plot3(tempPillar(:,1)-(cntrPt(1,1)-fSizeXmin),tempPillar(:,2)-(cntrPt(1,2)-fSizeYmin),tempPillar(:,6),'g')
+        end
+        waitfor(pPF,'UserData')
+        choice = pPF.UserData;
+        if choice == -1
+            problems(l,4) = 1;
+        elseif choice == -2
+        else
+            %         if size(find(lub(:,7)==choice & lub(:,6)>=lub(pPilIdx,6)),1)>0
+            %         replace = find(lub(:,7)==choice & lub(:,6)>=lub(pPilIdx,6));
+            %         lub(replace,7) = max(lub(:,7)+1);
+            %         end
+            lub(problems(pPilIdcs,1),7) = choice;
+            problems(pPilIdcs,3) = 1;
+        end
+        close()                                  
+    end
 end
-sLocs = unique(sLocs,'rows','stable');
-sLocs(sLocs(:,3)==0,:) = [];
-lub(:,23) = lub(:,7);
 
-[lub] = fixProblemPillars(lub,maxAi,problems,sLocs,maxD);
+delete = find(problems(:,4)==1);
+lub(problems(delete,1),:) = [];
 
-
-end
-
+lub = sortrows(lub,[7 6]);
 [lub] = refreshData(lub,maxAi,maxAc,numPillars,maxMasks);
 problems = find(lub(:,20)>0);
+end
 %%
-% Visualizing Problem Pillars
-figure
-imshow(maxMasks)
-hold on
-scatter3(lub(problems,1),lub(problems,2),lub(problems,6))
-clear tempInd1 tempInd2
-for j = 1:max(lub(:,7))
-    clear tempPillar
-    tempInd1 = find(lub(:,7)==j,1,'first');
-    tempInd2 = find(lub(:,7)==j,1,'last');
-    if((tempInd2-tempInd1)>0)
-    tempPillar = lub(tempInd1:tempInd2,:);
-    plot3(tempPillar(:,1),tempPillar(:,2),tempPillar(:,6))
-    %plot3(tempPillar(:,24),tempPillar(:,25),tempPillar(:,6))
+%Duplicate Point Correction
+%Rationale: Some objects appear in two frames at once, typically at the
+%junction between one ellipsoid and the next when there is large shear
+%deformation. The following code should give the user the option to merge
+%duplicate points (average their location) or choose a single one for a
+%problem pillar.
+
+%Simple Method
+for i = 1:size(lub,1)-1
+    if lub(i,6) == lub(i+1,6) && lub(i,7) == lub(i+1,7)
+        lub(i,:) = (lub(i,:) + lub(i+1,:))./2;
+        lub(i+1,:) = lub(i,:);
+        i
     end
 end
-hold off
-%% Automated Problem Pillar Selection with Manual Track Adjustment
-for l = 1:size(problems,1)
-    clear cntrPt pPilIdx
-    pPilIdx = problems(l,1);
-    cntrPt = round(lub(pPilIdx,1:2));
-    fSize = 50; % Distance from center point to make frame
-    nSize = 30; % max neighbor distance
-    
-    %determine if object is near the edge of the main frame
-    if size(maxMasks,2)-cntrPt(1,1) < fSize
-        fSizeXmin = fSize;fSizeXmax = (size(maxMasks,2)-cntrPt(1,1))-1;
-    elseif cntrPt(1,1) < fSize
-        fSizeXmin = cntrPt(1,1)-1;fSizeXmax = fSize;
-    else
-        fSizeXmin = fSize;fSizeXmax = fSize;
-    end
-    
-    if size(maxMasks,1)-cntrPt(1,2) < fSize
-        fSizeYmin = fSize;fSizeYmax = (size(maxMasks,1)-cntrPt(1,2))-1;
-    elseif cntrPt(1,2) < fSize
-        fSizeYmin = cntrPt(1,2)-1;fSizeYmax = fSize;
-    else
-        fSizeYmin = fSize;fSizeYmax = fSize;
-    end
 
-    pPF = figure('Position',[100 100 800 800]);
-    handles.axes1 = axes( ...
-        'Units','normalized', ...
-        'Position',[0 0 1 1]);
-    imshow(maxMasks(cntrPt(1,2)-fSizeYmin:cntrPt(1,2)+fSizeYmax,cntrPt(1,1)-fSizeXmin:cntrPt(1,1)+fSizeXmax))
-    hold on
-    pbd = uicontrol('style','pushbutton','units','pixels','position',[120,5,70,20],'string','delete','Callback',@pbDelete);
-    pbs = uicontrol('style','pushbutton','units','pixels','position',[120,25,70,20],'string','skip','Callback',@pbSkip);
-    scatter3(fSizeYmin,fSizeXmin,lub(pPilIdx,6))
-    nghbrs = unique(lub((lub(:,1)>(cntrPt(1,1)-nSize)&lub(:,1)<(cntrPt(1,1)+nSize)&lub(:,2)>(cntrPt(1,2)-nSize)&lub(:,2)<(cntrPt(1,2)+nSize)),7));
-    for i = 1:size(nghbrs,1)
-        if size(find(lub(:,7)==nghbrs(i,1) & lub(:,6)<lub(pPilIdx,6),1,'last'),1)>0
-            nghbrs(i,2) = find(lub(:,7)==nghbrs(i,1) & lub(:,6)<lub(pPilIdx,6),1,'last');
-            scatter3(lub(nghbrs(i,2),1)-(cntrPt(1,1)-fSizeYmin),lub(nghbrs(i,2),2)-(cntrPt(1,2)-fSizeXmin),lub(nghbrs(i,2),6),'r')
-            trckNum = num2str(nghbrs(i,1));
-            tempInd1 = find(lub(:,7)==nghbrs(i,1),1,'first');
-            tempInd2 = find(lub(:,7)==nghbrs(i,1),1,'last');
-            if((tempInd2-tempInd1)>0)
-                tempPillar = lub(tempInd1:tempInd2,:);
-                plot3(tempPillar(:,1)-(cntrPt(1,1)-fSizeYmin),tempPillar(:,2)-(cntrPt(1,2)-fSizeXmin),tempPillar(:,6))
-                %plot3(tempPillar(:,24),tempPillar(:,25),tempPillar(:,6))
-            end
-            pb(i) = uicontrol('style','pushbutton','units','pixels','position',[40,5+(20*i),70,20],'string',num2str(trckNum),'Callback',@pbPillarChoice);
-            
-            trckText = strcat('\leftarrow ',trckNum);
-            text(lub(nghbrs(i,2),1)-(cntrPt(1,1)-fSizeYmin),lub(nghbrs(i,2),2)-(cntrPt(1,2)-fSizeXmin),lub(nghbrs(i,2),6),trckText,'Color','red')
-        else
-            nghbrs(i,2) = 0;
-        end
-    end
-    waitfor(pPF,'UserData')
-    choice = pPF.UserData;
-    if choice == -1
-        lub(pPilIdx,:) = [];
-    elseif choice == -2
-    else
-        if size(find(lub(:,7)==choice & lub(:,6)>=lub(pPilIdx,6)),1)>0
-        replace = find(lub(:,7)==choice & lub(:,6)>=lub(pPilIdx,6));
-        lub(replace,7) = max(lub(:,7)+1);
-        end
-    lub(pPilIdx,7) = choice;
-    end
-    close()
-    
-    
-
-%     lub(lub(:,7)==str2double(rPil{1,1}) & lub(:,6)>lub(pPilIdx,6),7) = lub(pPilIdx,7);
-
-
-end
-     lub = sortrows(lub,[7 6]);
-     close all
-     plotTracks(lub,maxMasks);
+%%
+[lub2,a,b] = unique(lub,'rows');
+lub = lub2(b,:);
+plotTracks(lub,maxMasks);
 %%
 
 %% Manual Problem Pillar Selection with Manual Track Adjustment
-
-w = questdlg('Would you like to correct tracking errors?',...
-    'Manual Adjustments (Optional)','Yes','No','No');
-waitfor(w);
-if strcmp(w,'Yes') == 1
-    while strcmp(w,'Yes') == 1
-    close all
-    plotTracks(lub,maxMasks)
-    hold on
-%w = questdlg('Select a region containing incorrect tracking. Double-Click to confirm selection.',...
-%    'Select problem area','Okay','Okay');
-    [~,pBds]=imcrop;
-    close()
-    pBds = round(pBds);
-    pBds(1,3) = pBds(1,3)+pBds(1,1);
-    pBds(1,4) = pBds(1,4)+pBds(1,2);
-    imshow(maxMasks(pBds(1,2):pBds(1,4),pBds(1,1):pBds(1,3)))
-    hold on
-    problemPillars = unique(lub(find(lub(:,1)>pBds(1,1)&lub(:,1)<pBds(1,3)&lub(:,2)>pBds(1,2)&lub(:,2)<pBds(1,4)),7));
-    
-for j = 1:size(problemPillars,1)
-    clear tempPillar
-    tempInd1 = find(lub(:,7)==problemPillars(j,1),1,'first');
-    tempInd2 = find(lub(:,7)==problemPillars(j,1),1,'last');
-    if((tempInd2-tempInd1)>0)
-    tempPillar = lub(tempInd1:tempInd2,:);
-    plot3(tempPillar(:,1)-pBds(1,1),tempPillar(:,2)-pBds(1,2),tempPillar(:,6))
-    trckNum = num2str(problemPillars(j,1));
-    trckText = strcat('\leftarrow ',trckNum);
-    text(tempPillar(1,1)-pBds(1,1),tempPillar(1,2)-pBds(1,2),trckText,'Color','green')
-    end
-end
-    pPil = inputdlg('Enter the pillar ID you would like to correct','Please provide pillar ID');
-    pPilIdx = find(lub(:,7)==str2double(pPil{1,1}),1,'last');
-    cntrPt = round(lub(pPilIdx,1:2));
-    fSize = 25;
-    figure
-    imshow(maxMasks(cntrPt(1,2)-fSize:cntrPt(1,2)+fSize,cntrPt(1,1)-fSize:cntrPt(1,1)+fSize))
-    hold on
-    nghbrs = unique(lub((lub(:,1)>(cntrPt(1,1)-25)&lub(:,1)<(cntrPt(1,1)+25)&lub(:,2)>(cntrPt(1,2)-25)&lub(:,2)<(cntrPt(1,2)+25)),7));
-    for i = 1:size(nghbrs,1)
-        if size(find(lub(:,7)==nghbrs(i,1) & lub(:,6)>lub(pPilIdx,6),1,'first'),1)>0
-        nghbrs(i,2) = find(lub(:,7)==nghbrs(i,1) & lub(:,6)>lub(pPilIdx,6),1,'first');
-        plot(lub(nghbrs(i,2),1),lub(nghbrs(i,2),2))
-        trckNum = num2str(nghbrs(i,1));
-        trckText = strcat('\leftarrow ',trckNum);
-        text(lub(nghbrs(i,2),1)-(cntrPt(1,1)-25),lub(nghbrs(i,2),2)-(cntrPt(1,2)-25),trckText,'Color','green')
-        else
-        nghbrs(i,2) = 0;
-        end
-    end
-    rPil = inputdlg('Enter the replacement ID','Please provide pillar ID');
-    lub(lub(:,7)==str2double(rPil{1,1}) & lub(:,6)>lub(pPilIdx,6),7) = lub(pPilIdx,7);
-    lub = sortrows(lub,[7 6]);
-    w = questdlg('Fix another error?',...
-    'Manual Adjustments (Optional)','Yes','No','Yes');
-    end
-    close all
-    plotTracks(lub,maxMasks);
-end
+% 
+% w = questdlg('Would you like to correct tracking errors?',...
+%     'Manual Adjustments (Optional)','Yes','No','No');
+% waitfor(w);
+% if strcmp(w,'Yes') == 1
+%     while strcmp(w,'Yes') == 1
+%         close all
+%         plotTracks(lub,maxMasks)
+%         hold on
+%         %w = questdlg('Select a region containing incorrect tracking. Double-Click to confirm selection.',...
+%         %    'Select problem area','Okay','Okay');
+%         [~,pBds]=imcrop;
+%         close()
+%         pBds = round(pBds);
+%         pBds(1,3) = pBds(1,3)+pBds(1,1);
+%         pBds(1,4) = pBds(1,4)+pBds(1,2);
+%         imshow(maxMasks(pBds(1,2):pBds(1,4),pBds(1,1):pBds(1,3)))
+%         hold on
+%         problemPillars = unique(lub(find(lub(:,1)>pBds(1,1)&lub(:,1)<pBds(1,3)&lub(:,2)>pBds(1,2)&lub(:,2)<pBds(1,4)),7));
+%         
+%         for j = 1:size(problemPillars,1)
+%             clear tempPillar
+%             tempInd1 = find(lub(:,7)==problemPillars(j,1),1,'first');
+%             tempInd2 = find(lub(:,7)==problemPillars(j,1),1,'last');
+%             if((tempInd2-tempInd1)>0)
+%                 tempPillar = lub(tempInd1:tempInd2,:);
+%                 plot3(tempPillar(:,1)-pBds(1,1),tempPillar(:,2)-pBds(1,2),tempPillar(:,6))
+%                 trckNum = num2str(problemPillars(j,1));
+%                 trckText = strcat('\leftarrow ',trckNum);
+%                 text(tempPillar(1,1)-pBds(1,1),tempPillar(1,2)-pBds(1,2),trckText,'Color','green')
+%             end
+%         end
+%         pPil = inputdlg('Enter the pillar ID you would like to correct','Please provide pillar ID');
+%         pPilIdx = find(lub(:,7)==str2double(pPil{1,1}),1,'last');
+%         cntrPt = round(lub(pPilIdx,1:2));
+%         fSize = 25;
+%         figure
+%         imshow(maxMasks(cntrPt(1,2)-fSize:cntrPt(1,2)+fSize,cntrPt(1,1)-fSize:cntrPt(1,1)+fSize))
+%         hold on
+%         nghbrs = unique(lub((lub(:,1)>(cntrPt(1,1)-25)&lub(:,1)<(cntrPt(1,1)+25)&lub(:,2)>(cntrPt(1,2)-25)&lub(:,2)<(cntrPt(1,2)+25)),7));
+%         for i = 1:size(nghbrs,1)
+%             if size(find(lub(:,7)==nghbrs(i,1) & lub(:,6)>lub(pPilIdx,6),1,'first'),1)>0
+%                 nghbrs(i,2) = find(lub(:,7)==nghbrs(i,1) & lub(:,6)>lub(pPilIdx,6),1,'first');
+%                 plot(lub(nghbrs(i,2),1),lub(nghbrs(i,2),2))
+%                 trckNum = num2str(nghbrs(i,1));
+%                 trckText = strcat('\leftarrow ',trckNum);
+%                 text(lub(nghbrs(i,2),1)-(cntrPt(1,1)-25),lub(nghbrs(i,2),2)-(cntrPt(1,2)-25),trckText,'Color','green')
+%             else
+%                 nghbrs(i,2) = 0;
+%             end
+%         end
+%         rPil = inputdlg('Enter the replacement ID','Please provide pillar ID');
+%         lub(lub(:,7)==str2double(rPil{1,1}) & lub(:,6)>lub(pPilIdx,6),7) = lub(pPilIdx,7);
+%         lub = sortrows(lub,[7 6]);
+%         w = questdlg('Fix another error?',...
+%             'Manual Adjustments (Optional)','Yes','No','Yes');
+%     end
+%     close all
+%     plotTracks(lub,maxMasks);
+% end
 
 
 
@@ -322,8 +360,8 @@ for j = 1:max(lub(:,7))
     tempInd1 = find(lub(:,7)==j,1,'first');
     tempInd2 = find(lub(:,7)==j,1,'last');
     if((tempInd2-tempInd1)>0)
-    tempPillar = lub(tempInd1:tempInd2,:);
-    plot3(tempPillar(:,1),tempPillar(:,2),tempPillar(:,6))
+        tempPillar = lub(tempInd1:tempInd2,:);
+        plot3(tempPillar(:,1),tempPillar(:,2),tempPillar(:,6))
     end
 end
 hold off
@@ -333,9 +371,20 @@ figure
 imshow(maxMasks)
 hold on
 scatter3(lub(55516:end,1),lub(55516:end,2),lub(55516:end,6))
+%% Remove Pillars Smaller than Threshold
+sT = 10;
+numPillars = unique(lub(:,7));
+for i = 1:size(numPillars)
+     tempInd1 = find(lub(:,7)==numPillars(i,1));
+     if size(tempInd1,1) <= sT
+         lub(tempInd1,:)=[];
+     end
+end
+
+
 %% Creating Trajectories Input text file
 disp('Creating Text File for trajectories.m.')
-
+clear subpixMaxima2
 %Organize subpixMaxima2 to be compatible with trajectories script
 subpixMaxima2(:,1)=1:1:size(lub,1);
 subpixMaxima2(:,2)=lub(:,1);
@@ -371,8 +420,8 @@ for j = 1:max(lub(:,7))
     tempInd1 = find(lub(:,7)==j,1,'first');
     tempInd2 = find(lub(:,7)==j,1,'last');
     if((tempInd2-tempInd1)>0)
-    tempPillar = lub(tempInd1:tempInd2,:);
-    plot3(tempPillar(:,1),tempPillar(:,2),tempPillar(:,6))
+        tempPillar = lub(tempInd1:tempInd2,:);
+        plot3(tempPillar(:,1),tempPillar(:,2),tempPillar(:,6))
     end
 end
 hold off
