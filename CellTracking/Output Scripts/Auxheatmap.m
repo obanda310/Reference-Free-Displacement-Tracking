55c1fd853a8e7aa8677351824efa136013793617
obanda310 <obanda310@gmail.com>
2018-04-17 14:44:34 -0400

    Adds generic function to create heat maps
    For viewing outputs of Franck Lab code

---
+++ CellTracking/Output Scripts/Auxheatmap.m
@@ -0,0 +1,74 @@
+function [heatmap] = Auxheatmap(xq,yq,vq,LUT,filename,savePath,maxD)
+if nargin == 6
+maxD = max(abs(vq(:)));
+end
+
+%Check to see if data includes direction or is simply magnitudes
+sidedCheck = min(vq(:))<0;
+
+%Build heat map accordingly
+if sidedCheck == 1
+    %doublesided
+    scaleD = 32768/maxD;
+    map = brewermap(65535,LUT);
+    hm = imagesc(xq,yq,vq);
+    hm2 = hm.CData;
+    hm2(hm2>0) = 32768+(abs(hm2(hm2>0))*scaleD);
+    hm2(hm2<0) = 32768 - (abs(hm2(hm2<0))*scaleD);
+    hm2(hm2==0) = 32768;
+    hm2(isnan(hm2)) = 32768;
+    hm2 = uint16(hm2);
+else
+    %singlesided
+    scaleD = 65535/maxD;
+    map = brewermap(65535,LUT);
+    hm = imagesc(xq,yq,vq);
+    hm2 = hm.CData;
+    hm2 =(hm2*scaleD);
+    hm2(isnan(hm2)) = 0;
+    hm2 = uint16(hm2);
+end
+imageHeatColor = single(ind2rgb(hm2,map));
+heatmap(:,:,:) = imageHeatColor;
+close all
+maxHeatMap = figure;
+hold on
+imshow(imageHeatColor);
+
+%% Export Image
+savefile = [savePath filename '.tif'];
+export_fig(maxHeatMap,savefile,'-native');
+
+%% Colorbar
+mkdir('HeatMaps\Traction','ColorBar')
+
+
+colorBar1 = single(zeros(500,25));
+range = uint16(round(linspace(65536,1,500)'));
+for i = 1:25
+    colorBar1(1:500,i) = range;
+end
+colorBar2 = ind2rgb(colorBar1,map);
+for i = 1:10
+    colorBar2((i*50)-3:(i*50),13:25,:) = 0;
+end
+
+colorBar2(1:3,13:25,:) = 0;
+%Save Color Bar Image
+close all
+colorBarSave = figure;
+hold on
+imshow(colorBar2);
+savefile = [savePath '\ColorBar\' filename 'ColorBar.tif'];
+export_fig(colorBarSave,savefile,'-native');
+
+filePath=cd;
+cd HeatMaps\Traction\ColorBar
+colorBarTxt = fopen([filename 'ColorBarValues.txt'],'wt');
+p1Format = 'Tick number %1.0f is %.2f \n';
+for i = 1:11
+    colorBarValues(i,1) =  round(maxD - ((maxD/5)*(11-i)),2); % round(max(vq(:)) - ((max(vq(:))/10)*(11-i)),2)
+    fprintf(colorBarTxt,p1Format,i,colorBarValues(i,1));
+end
+fclose(colorBarTxt);
+cd(filePath)
