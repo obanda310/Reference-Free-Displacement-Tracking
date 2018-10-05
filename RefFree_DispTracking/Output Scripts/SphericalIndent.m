c2768f295c3c1829824e0d9654a803bac09569dd
obanda310 <obanda310@gmail.com>
2018-10-05 13:25:41 -0400

    Updates indentation analysis protocol

--- RefFree_DispTracking/Output Scripts/SphericalIndent.m
+++ RefFree_DispTracking/Output Scripts/SphericalIndent.m
@@ -1,18 +1,32 @@
-function SphericalIndent(directory)
-if nargin ==1
-    cd(directory);
-end
-
-%% Making the spherical indentation data for FE fitting
+% function SphericalIndent(directory)
+% if nargin ==1
+%     cd(directory);
+% end
 clear all
 close all
+%%
+%CHANGE FONT SIZES HERE
+AxisFontSize = 24;
+AxisTitleFontSize = 24;
+LegendFontSize = 14;
+
+colOptions{1,1} = 'white';
+colOptions{2,1} = 'black';
+colOptions{1,2} = 'black';
+colOptions{2,2} = 'white';
+%% Making the spherical indentation data for FE fitting
+
 load('3Ddata.mat')
 tic
-try
-    load SphereIndent
-catch
-    IndPlanes = [1 2 4];    
+for i = 1:size(planesGroups,1)
+    pgIdx = find(planesGroups(i,:));
+    pgLocs(i,1) = mean(planesLoc2(planesGroups(i,pgIdx)));
 end
+topPlane = find(pgLocs==min(pgLocs));
+
+
+    IndPlanes = [1:size(vq3,3)];    
+
 vqtest = vq3(:,:,IndPlanes);
 h = fspecial('average', [20 20]);
 vqtest(isnan(vqtest)) = 0;
@@ -25,41 +39,115 @@ for i = 1:size(vqtest,3)
 end
 vqtestfilt3 = vqtestfilt2*-1;
 ShowStack(vqtestfilt3)
-%%
-vqtestproj = squeeze(mean(permute(vqtestfilt3,[3,1,2])));
-%%
 
 %%
+figure
+
+
+
+imshow(sum(vqN,3,'omitnan'),[])
+for i =1:5
+vqtestproj = sum(vq3,3,'omitnan')*-1;
+vqCO = prctile(vqtestproj(:),i+91);
+vqtestproj(vqtestproj<vqCO) = 0;
+
+cmap = brewermap(25,'blues');
+
 %inputs:
 %originalmatrix: the original matrix
 %binarisedmatrix = originalmatrix > threshold; %the thresholded matrix, a logical array
 [rows, cols] = ndgrid(1:size(vqtestproj, 1), 1:size(vqtestproj, 2));
-rc = sum(rows(vqtestproj>0) .* vqtestproj(vqtestproj>0)) / sum(vqtestproj(vqtestproj>0));
-cc = sum(cols(vqtestproj>0) .* vqtestproj(vqtestproj>0)) / sum(vqtestproj(vqtestproj>0));
-%%
-imshow(vqtestproj,[])
+rc(i) = sum(rows(vqtestproj>0) .* vqtestproj(vqtestproj>0)) / sum(vqtestproj(vqtestproj>0));
+cc(i) = sum(cols(vqtestproj>0) .* vqtestproj(vqtestproj>0)) / sum(vqtestproj(vqtestproj>0));
+
+
 hold on
-scatter(cc,rc,50)
+scatter(cc(i),rc(i),50,'MarkerEdgeColor',cmap(i,:),'linewidth',2)
+end
+
+rcf = round(mean(rc));
+ccf = round(mean(cc));
+%% Remove planes where center data doesn't exist
+clear planeCounts planeCountsCO
+%Identify which planes have fewer members than others
+for i = 1:size(planesGroups,1)
+    planeCounts(i,1) = (nnz(plane.final(:,planesGroups(i,1:nnz(planesGroups(i,:))))));
+end
+pCMax = max(planeCounts);
+planeCounts(:,2) = planeCounts(:,1)/pCMax;
+
+planeCountsCO = planeCounts(:,2)<.8;
+
+pgLocs(planeCountsCO,:) = [];
+planesGroups(planeCountsCO,:) = [];
+vqtestfilt3(:,:,planeCountsCO) = [];
+IndPlanes = [1:size(vqtestfilt3,3)];
+
+%% Create map of distances to center point
+
+distvq = zeros(size(vqN,1),size(vqN,2));
+distvq(rcf,ccf)=1;
+distvq=round(bwdist(distvq));
+figure
+imshow(distvq,[])
+hold on
+scatter(cc,rc,50,'MarkerEdgeColor','r','linewidth',2)
 
 %%
-for j = 1:size(vqtestfilt3,3)
-    for i = 1:500
-        vqmask2 = double(not(vqtestfilt3(:,:,j)>=0));
-        vqmask2 = rgb2gray(insertShape(vqmask2,'circle',[cc rc i],'LineWidth',1))>0;
-        vqmask2area = sum(sum(single(vqmask2>0)));
-        radprofile(i,j) = sum(sum(vqmask2.*vqtestfilt3(:,:,j)))/vqmask2area;
-        i
+td = max(distvq(:));
+
+tic
+for j = 1:size(pgLocs,1)
+    distvqmask = (abs(vqtestfilt3(:,:,j))>0).*distvq;
+    for i = 1:td
+        distvqA = sum(sum(distvqmask==i));
+        radprofile(i,j) = sum(sum((distvq==i).*vqtestfilt3(:,:,j)))/distvqA;
     end
+    toc
 end
+
 %%
+    fcolor = colOptions{2,1};
+    bcolor = colOptions{2,2};
+set(0,'defaultfigurecolor',bcolor)
 figure
 hold on
 
-for i = 1:size(vqtestfilt3,3)
-    plot(radprofile(:,i))
+[~,order] = sort(pgLocs,1);
+    
+for i = 1:size(order)
+    plot([1:1:size(radprofile,1)]*.1625,radprofile(:,order(i)),'DisplayName',num2str(round(pgLocs(order(i),1),1)))
+    
 end
+ 
+    
+    set(gca,'Color',bcolor,'LineWidth',2)
+    %Axes, Text, Legends
+    ylim([0 ceil(max(radprofile(:)))+1])
+    set(gca,'fontsize',AxisFontSize,'XColor',fcolor,'YColor',fcolor,'YMinorTick','on')
+    ytickformat('%.1f')
+    xt = 'Radial Distance (\mum)';% input('enter the xaxis label','s');
+    yt = 'Displacement (\mum)'; %input('enter the yaxis label','s');
+    tt = 'Line-Profile Displacements';%input('enter the title','s');
+    xl = xlabel(xt);
+    yl = ylabel(yt);
+    %tl = title(tt);
+    
+    set(xl, 'fontweight','bold','fontsize',28,'color',fcolor);
+    set(yl,'fontweight','bold','fontsize',28,'color',fcolor);
+    legend show
+    legend boxoff
+    leg.FontSize = LegendFontSize;
+    %set(tl,'fontweight','bold','fontsize',title_font_size)
+    
+    
+    %Export Image
+%     title = ['\ShearVsArea ' fcolor ' on ' bcolor];
+%     savefile = [ListPath title];
+%     export_fig(sheararea,savefile,'-native');
 
 %%
+clear radprofile2 radprofile3 radprofileXs radprofileZs
 for j = 1:size(planesGroups,1)
     planesLocs3(j) = mean(planesLoc2(1,planesGroups(j,1:nnz(planesGroups(j,:)))));
 end
@@ -68,13 +156,15 @@ for i = 1:size(vqtestfilt3,3)
     radprofile2(:,i) = radprofile(:,i) + planesLocs4(i);
 end
 radprofile3 = double(radprofile(:,1:size(IndPlanes,2)));
-radprofileXs = (1:500)' * raw.dataKey(9,1) *ones(1,size(IndPlanes,2));
-radprofileZs = double(ones(500,1) * planesLocs4  );
+radprofileXs = double((1:td)' * raw.dataKey(9,1) *ones(1,size(IndPlanes,2)));
+radprofileZs = double(ones(td,1) * planesLocs4  );
 
-[radXX,radYY] = meshgrid(([1:1:500]' * raw.dataKey(9,1)), min(planesLocs4):(max(planesLocs4)-min(planesLocs4))/50:max(planesLocs4));
+[radXX,radYY] = meshgrid(([1:1:td]' * raw.dataKey(9,1)), min(planesLocs4):(max(planesLocs4)-min(planesLocs4))/50:max(planesLocs4));
+radXX = double(radXX);
 radYY = double(radYY);
 radvq = griddata(radprofileXs,radprofileZs,radprofile3,radXX,radYY);
 %% Heatmap for XZ
+figure
 colorMapRad = brewermap(65536,'spectral');
 MaximumHeatMap = imagesc(radXX(:,1),radYY(:,1),radvq);
 radHeat = MaximumHeatMap.CData;%.*(imageBinary==0);
@@ -90,5 +180,5 @@ imshow(radHeatColor)
 hold on
 toc
 %%
-save('SphereIndent.mat','radHeatColor','radprofile3','radprofileXs','radprofileZs','radYY','radXX','radvq','IndPlanes','planesLocs4')
+save('SphereIndent.mat','radHeatColor','radprofile3','radprofileXs','radprofileZs','radYY','radXX','radvq','IndPlanes','planesLocs4','cc','rc','vqtestproj')
 %plot([5 10 15 20 25 30 35 40 45 50 55 60 65 70]/xyScale,[1 50 1 50 1 50 1 50 1 50 1 50 1 50] )
