db2966ba3df5b00aac1abe9daebcf98bde6574a2
obanda310 <obanda310@gmail.com>
2019-01-21 15:30:15 -0500

    Updates function to automatically scale up images
    also corrects color bar values in some cases

--- RefFree_DispTracking/Output Scripts/Auxheatmap.m
+++ RefFree_DispTracking/Output Scripts/Auxheatmap.m
@@ -1,10 +1,13 @@
-function [heatmap] = Auxheatmap(xq,yq,vq,LUT,filename,savePath,maxD)
+function [heatmap] = Auxheatmap(xqMax,yqMax,vqIni,LUT,filename,savePath,maxD,CO)
 if nargin == 6
 maxD = max(abs(vq(:)));
 end
-
+xq = 1:1:xqMax;
+yq = 1:1:yqMax;
+vq = imresize(vqIni,[xqMax yqMax]);
+vq(abs(vq)<CO) = 0;
 %Check to see if data includes direction or is simply magnitudes
-sidedCheck = min(vq(:))<0;
+sidedCheck = min(vqIni(:))<0;
 
 %Build heat map accordingly
 if sidedCheck == 1
@@ -22,6 +25,9 @@ else
     %singlesided
     scaleD = 65535/maxD;
     map = brewermap(65535,LUT);
+%     if strcmp(LUT,'*spectral')==1
+%       map(1,:) = [0,0,0];  
+%     end
     hm = imagesc(xq,yq,vq);
     hm2 = hm.CData;
     hm2 =(hm2*scaleD);
@@ -30,7 +36,7 @@ else
 end
 imageHeatColor = single(ind2rgb(hm2,map));
 heatmap(:,:,:) = imageHeatColor;
-close all
+close
 maxHeatMap = figure;
 hold on
 imshow(imageHeatColor);
@@ -55,20 +61,30 @@ end
 
 colorBar2(1:3,13:25,:) = 0;
 %Save Color Bar Image
-close all
+
 colorBarSave = figure;
 hold on
 imshow(colorBar2);
 savefile = [savePath '\ColorBar\' filename 'ColorBar.tif'];
 export_fig(colorBarSave,savefile,'-native');
 
+close
+
 filePath=cd;
 cd HeatMaps\Traction\ColorBar
 colorBarTxt = fopen([filename 'ColorBarValues.txt'],'wt');
 p1Format = 'Tick number %1.0f is %.2f \n';
-for i = 1:11
-    colorBarValues(i,1) =  round(maxD - ((maxD/5)*(11-i)),2); % round(max(vq(:)) - ((max(vq(:))/10)*(11-i)),2)
-    fprintf(colorBarTxt,p1Format,i,colorBarValues(i,1));
+if sidedCheck == 1
+    for i = 1:11
+        colorBarValues(i,1) =  round(maxD - ((maxD/5)*(11-i)),2); % round(max(vq(:)) - ((max(vq(:))/10)*(11-i)),2)
+        fprintf(colorBarTxt,p1Format,i,colorBarValues(i,1));
+    end
+else
+    for i = 1:11
+        colorBarValues(i,1) =  round(maxD - ((maxD/10)*(11-i)),2); % round(max(vq(:)) - ((max(vq(:))/10)*(11-i)),2)
+        fprintf(colorBarTxt,p1Format,i,colorBarValues(i,1));
+    end
+    
 end
 fclose(colorBarTxt);
 cd(filePath)
