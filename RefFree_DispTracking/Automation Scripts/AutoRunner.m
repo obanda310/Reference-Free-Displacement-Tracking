5cd4d5430382aa84e62a4f903be93f5847dc5b22
obanda310 <obanda310@gmail.com>
2019-05-06 10:40:54 -0400

    Update RefFree_DispTracking/Automation Scripts/AutoRunner.m
    Adds method to view many traction heat  maps simultaneously

--- RefFree_DispTracking/Automation Scripts/AutoRunner.m
+++ RefFree_DispTracking/Automation Scripts/AutoRunner.m
@@ -106,13 +106,13 @@ clear failedTractions TractionStats
 failed = 0;
 for i = 1:size(dirList,1)
     try
-    disp(['Trying ' num2str(i)])
-    [TractionStats(i,:)] = interpFinal3D_2(dirList{i,1});
+        disp(['Trying ' num2str(i)])
+        [TractionStats(i,:)] = interpFinal3D_2(dirList{i,1});
     catch
         disp(['Conversion Failed at ' num2str(i)])
         failedTractions(i) = i;
     end
-
+    
 end
 TractionStats(:,9:11) = TractionStats(:,4:6)*1000000000;
 save([ListPath prefix 'TractionStats.mat'],'TractionStats','failed')
@@ -122,7 +122,7 @@ results2 = TractionStats;
 figure; scatter(results2(:,4),results2(:,5))
 A = mean(results2(:,4)./results2(:,5),1,'omitnan')
 
-    
+
 %% Collect vqXY/vqZ totals for shear v normal graphs
 %goodData = [1;2;3;5;6;14;17;23;31;34;43;44;47;49;50;48;11;12;19;25;28;36];
 %goodData = [1:1:51]';
@@ -133,37 +133,39 @@ clear compZ
 for m = 1:size(goodData,1)
     
     try
-        clear planesLoc3
-        disp(dirList{goodData(m,1),1})
-        cd(dirList{goodData(m,1),1});
-        load('3Ddata.mat')
-        load('Profile Data\vqXY.mat');
-        load('Profile Data\HeatMapXY.mat');
-        %load('SurfaceData.mat','vqZt');
-        load('Inputs2.mat','u')
-        zTarget = 4;
-        for j = 1:size(planesGroups,1)
-            planesLoc3(j) = mean(planesLoc2(1,planesGroups(j,1:nnz(planesGroups(j,:)))));
-        end
-        zPlane = find(abs(planesLoc3-zTarget) == min(abs(planesLoc3-zTarget)),1,'first');
-        vqXY(isnan(vqXY)) = 0;
-        vqXY = imresize(vqXY,[size(vq3,1) size(vq3,2)]);
-        m
-        vqT(m,1) = sum(sum(vqXY));
-        vq3(isnan(vq3)) = 0;
-        vqT(m,2) = sum(sum(abs(u{1}{3}(:,:,end-1))))/(1*10^-6);
-        vqT(m,3) = zPlane;
-        vqT(m,4) = planesLoc2(zPlane);
-        vqT(m,5) = sum(sum(image.Area ==0))*raw.dataKey(9,1)^2;
-        vqT(m,6) = max(max(vqXY));
-        vqT(m,7) = max(max(abs(vq3(:,:,zPlane))));
-        
-        compZ(1:500,1:500,m) = imresize(vq3(:,:,zPlane),[500 500],'nearest');
-        compZ(501:1000,1:500,m) = imresize(vqZt(:,:),[500 500],'nearest');
-        
-    catch
-        disp('failed')
+    clear planesLoc3
+    disp(dirList{goodData(m,1),1})
+    cd(dirList{goodData(m,1),1});
+    load('3Ddata.mat')
+    load('Profile Data\vqXY.mat');
+    load('Profile Data\HeatMapXY.mat');
+    %load('SurfaceData.mat','vqZt');
+    load('Inputs2.mat','u')
+    zTarget = 4;
+    for j = 1:size(planesGroups,1)
+        planesLoc3(j) = mean(planesLoc2(1,planesGroups(j,1:nnz(planesGroups(j,:)))));
     end
+    zPlane = find(abs(planesLoc3-zTarget) == min(abs(planesLoc3-zTarget)),1,'first');
+    vqXY(isnan(vqXY)) = 0;
+    vqXY = imresize(vqXY,[size(vq3,1) size(vq3,2)]);
+    m
+    vqT(m,1) = sum(sum(vqXY));
+    vq3(isnan(vq3)) = 0;
+    vqT(m,2) = sum(sum(abs(u{1}{3}(:,:,end-1))))/(1*10^-6);
+    vqT(m,3) = zPlane;
+    vqT(m,4) = planesLoc2(zPlane);
+    vqT(m,5) = sum(sum(image.Area ==0))*raw.dataKey(9,1)^2;
+    vqT(m,6) = max(max(vqXY));
+    vqT(m,7) = max(max(abs(vq3(:,:,zPlane))));
+    
+    compZ(1:500,1:500,m) = imresize(vq3(:,:,zPlane),[500 500],'nearest');
+    compZ(501:1000,1:500,m) = imresize(vqZt(:,:),[500 500],'nearest');
+    
+    
+        catch
+            disp('failed')
+            m
+        end
 end
 
 vqT(:,8) = vqT(:,1)./vqT(:,2);
@@ -183,8 +185,94 @@ for i = 1:size(compZ,3)
     
     imwrite(tempImage,'CompSurfaceTo3D.tif','WriteMode','append');
 end
-    
 
+%% Look at Traction Data
+goodData = [1:1:size(dirList,1)]';
+clear vqT
+clear compT
+close all
+for m = 1:size(goodData,1)
+    try
+        clear planesLoc3
+        disp(dirList{goodData(m,1),1})
+        cd(dirList{goodData(m,1),1});
+        load('TractionOutputs.mat')
+        compTi(1:500,1:500,m) = imresize(tiPN{1}{1},[500 500],'nearest');
+        compTi(501:1000,1:500,m) = imresize(tiPN{1}{2},[500 500],'nearest');
+        tFig = figure
+        hold on
+        imshow(compTi(:,:,m),[])
+        t = text(.05,50,num2str(m),'FontSize',40,'FontWeight','bold','Color','white');
+        tFigImage = getframe(gca,[0 100 280 560]);
+        compT(:,:,m) = rgb2gray(tFigImage.cdata) ; 
+%         figure
+%         imshow(tFigImage.cdata,[])
+    catch
+        disp('failed')
+        m
+    end
+end
+compT(isnan(compT)) = 0;
+compT = double(compT);
+compT2 = (compT+(-1*min(compT(:))));
+compT2 = (compT2/max(compT2(:)))*256;
+for i = 1:size(compT,3)
+    clear tempImage
+    tempImage(:,:) = uint8(compT2(:,:,i));    
+    imwrite(tempImage,[ListPath prefix 'CompTractions.tif'],'WriteMode','append');
+end
+%% Make Montage Figures
+goodData = [1:1:size(dirList,1)]';
+close all
+clear temp temp2 shearMontage h hr hi
+for m = 1:size(goodData,1)
+%     try       
+        disp(dirList{goodData(m,1),1})
+        cd(dirList{goodData(m,1),1});
+        if m == 1
+        shearMontage = imread('HeatMaps\Traction\ShearTractions.tif');        
+        h = size(shearMontage,1);
+        else
+        temp = imread('HeatMaps\Traction\ShearTractions.tif');
+        hi = size(temp,1);
+        hr = h/hi;
+        temp2 = imresize(temp,[h round(hr*size(temp,2))]);
+        shearMontage = cat(2,shearMontage,temp2);            
+        end
+%     catch
+%     end
+end
+shear = figure;
+imshow(shearMontage)
+
+    title = ['Shear Montage'];
+    savefile = [ListPath title];
+    export_fig(shear,savefile,'-native');
+
+clear temp temp2 normMontage h hr hi
+for m = 1:size(goodData,1)
+%     try       
+        disp(dirList{goodData(m,1),1})
+        cd(dirList{goodData(m,1),1});
+        if m == 1
+        normMontage = imread('HeatMaps\Traction\Ztractions.tif');        
+        h = size(normMontage,1);
+        else
+        temp = imread('HeatMaps\Traction\Ztractions.tif');
+        hi = size(temp,1);
+        hr = h/hi;
+        temp2 = imresize(temp,[h round(hr*size(temp,2))]);
+        normMontage = cat(2,normMontage,temp2);            
+        end
+%     catch
+%     end
+end
+normal = figure;
+imshow(normMontage)
+
+    title = ['Normal Montage'];
+    savefile = [ListPath title];
+    export_fig(normal,savefile,'-native');
 
 %% For Running Spherical Indentation Script
 for i = 1:size(dirList,1)
@@ -222,8 +310,8 @@ for i = 1:size(dirList,1)
     else
         load('StackName.mat','StackName')
         %if StackName == 0
-            [StackName,StackPath] = uigetfile('*.tif');
-            save('StackName.mat','StackName')
+        [StackName,StackPath] = uigetfile('*.tif');
+        save('StackName.mat','StackName')
         %end
     end
     
@@ -315,4 +403,13 @@ xyzStats(2,2) = std(StdVals(:,4));
 
 xyzStats(2,3) = mean(StdVals((StdVals(:,4)<(xyzStats(2,1)+2*xyzStats(2,2))),4));
 xyzStats(2,4) = std(StdVals((StdVals(:,4)<(xyzStats(2,1)+2*xyzStats(2,2))),4));
+%% Determine Aspect Ratio of Cells
 
+for i = 1:size(dirList,1)
+    clear totals
+    AR = cellAspectRatio(dirList{i});
+    fAR(i,1:3) = AR;
+    i
+end
+
+save([ListPath prefix 'AspectRatios.mat'],'fAR')
